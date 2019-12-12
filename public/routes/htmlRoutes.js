a2f3369ebfecc6cda1d5f0148ee9e8948be3c45a
TheBrellis <brianroyalellis@gmail.com>
2019-12-12 18:11:59 -0500

    build htmlRoutes.js, setting up responses for server for all html get requests

---
+++ public/routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+// exporting html paths
+module.exports = function(app) {
+    
+    
+    app.get("/notes", function(req,res) {
+        res.sendFile(path.join(__direname, "../public/notes.html"));
+    })
+    
+    //set index.html as default page
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+};
